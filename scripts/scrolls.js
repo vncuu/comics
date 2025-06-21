48fd8d98912b9c980e02899db3932cabbb5e4ce5
vncuu <99640766+vncuu@users.noreply.github.com>
2025-06-21 11:14:02 +0300

    Update and rename script.js to scrolls.js

---
+++ scripts/scrolls.js
@@ -0,0 +1,30 @@
+const slider = document.querySelector('.main-bar');
+
+let isDown = false;
+let startX = 0;
+let scrollLeft = 0;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1.5; //Scroll speed
+  slider.scrollLeft = scrollLeft - walk;
+});
